22965a93674886345b097f3da1c17f2247eb47e9
lotusCupcake <fikriansari.mfa@gmail.com>
2025-06-05 23:02:39 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/story-app/sw.js', { scope: '/story-app/' })})}
